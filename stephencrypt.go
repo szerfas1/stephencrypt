bf265a7405218eeb6ecfa1ba7a46cb318684e292
szerfas <szerfas@lyft.com>
2021-01-25 10:04:12 -0800

    add a backwards-incompatible change with a go mod present this time

--- stephencrypt.go
+++ stephencrypt.go
@@ -14,22 +14,22 @@ package stephencrypt
 
 //const KEY = `STEPHEN`
 
-func StephenCrypt(clearText string) string {
+func StephenCrypt(clearText string, shift int32) string {
 	var result string
 	//var keyIndex int
 	//for i, v := range clearText {
 	for _, v := range clearText {
 		//keyIndex = i % len(KEY)
 		//result = result + string(v + int32(KEY[keyIndex]))
-		result = result + string(v + 1)
+		result = result + string(v + shift)
 	}
 	return result
 }
 
-func StephenDecrypt(cipherText string) string {
+func StephenDecrypt(cipherText string, shift int32) string {
 	var result string
 	for _, v := range cipherText {
-		result = result + string(v - 1)
+		result = result + string(v - shift)
 	}
 	return result
 }
