a18e3e76ac1af445816e3034c4164bdc9872d45b
szerfas <szerfas@lyft.com>
2021-01-25 09:11:17 -0800

    add stephencrypt

---
+++ stephencrypt.go
@@ -0,0 +1,35 @@
+//package main
+
+//import "fmt"
+//import stephencrypt "github.com/bradfield-csi-2020/szerfas_CSI/advanced_programming/6_packages_and_modules"
+//
+//func main() {
+//	input := "aaa"
+//	fmt.Printf("input text: %s\n", input)
+//	fmt.Printf("ciphered text: %s\n", stephencrypt.StephenCrypt(input))
+//	fmt.Printf("decciphereed text: %s\n", stephencrypt.StephenDecrypt(stephencrypt.StephenCrypt(input)))
+//}
+
+package stephencrypt
+
+//const KEY = `STEPHEN`
+
+func StephenCrypt(clearText string, shift int32) string {
+	var result string
+	//var keyIndex int
+	//for i, v := range clearText {
+	for _, v := range clearText {
+		//keyIndex = i % len(KEY)
+		//result = result + string(v + int32(KEY[keyIndex]))
+		result = result + string(v + shift)
+	}
+	return result
+}
+
+func StephenDecrypt(cipherText string, shift int32) string {
+	var result string
+	for _, v := range cipherText {
+		result = result + string(v - shift)
+	}
+	return result
+}
